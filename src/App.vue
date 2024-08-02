f9f011b0c720562cec5888f19d6866edd4cf9819
liwuyi <973574291@qq.com>
2024-08-03 00:57:03 +0800

    dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
 import { useStorage, onKeyStroke } from "@vueuse/core";
 import NavHeader from './components/nav-header.vue';
+import { useRoute } from "vue-router";
+import { computed } from "vue";
 const isFullscreen = useStorage('is-fullscreen', false);
 onKeyStroke('Escape', () => {
   isFullscreen.value = !isFullscreen.value;
+});
+const route = useRoute();
+const klass = computed(() => {
+  if (route.name === 'list') {
+    return '';
+  }
+  return route.path.split('/')[1];
 })
 </script>
 <template>
   <transition>
     <nav-header v-if="!isFullscreen"></nav-header>
   </transition>
-  <main>
+  <main :class="klass">
     <router-view></router-view>
   </main>
 </template>
---
+++ src/views/games/tetris.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>俄罗斯方块</div>
+</template>
